7438b9c8700df98b8ad4f6b47b3c9380f697b842
1BM17CS150 <54196731+1BM17CS150@users.noreply.github.com>
2019-10-10 14:49:14 +0530

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,36 +1,37 @@
 
 
+class validation:
+    def comp(self,k):
+        if k=='(':
+            return ')' 
+        if k=='{':
+            return '}'
+        if k=='[':
+            return ']'
 
-def comp(k):
-    if k=='(':
-        return ')' 
-    if k=='{':
-        return '}'
-    if k=='[':
-        return ']'
 
+    def validate(self,s):
+        l=[]
+        top=-1
+        for i in s:
+            if len(l)==0 and i in ("{","(","["): 
+               top=top+1
+               l.append(i)
 
-def validate(s):
-    l=[]
-    top=-1
-    for i in s:
-        if len(l)==0 and i in ("{","(","["): 
-           top=top+1
-           l.append(i)
-      
-        elif len(l)!=0 and (i==comp(l[top]) or (i in ("(","{","["))):
-              if i==comp(l[top]):
-                  l.pop()
-                  top=top-1
-              elif i in ("(","{","["):
-                top=top+1
-                l.append(i)
+            elif len(l)!=0 and (i==self.comp(l[top]) or (i in ("(","{","["))):
+                  if i==comp(l[top]):
+                      l.pop()
+                      top=top-1
+                  elif i in ("(","{","["):
+                    top=top+1
+                    l.append(i)
+            else:
+                print("Invalid")
+                return
+        if len(l)==0:
+            print("Valid")
         else:
             print("Invalid")
-            return
-    if len(l)==0:
-        print("Valid")
-    else:
-        print("Invalid")
 string=input("enter the string")
-validate(string)
+obj=validation()
+obj.validate(string)
